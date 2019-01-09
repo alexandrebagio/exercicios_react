8fc9e7e58fd6c97f1be99dbc3dd83cae930cd5f4
Alexandre Bagio da Luz <alexandre.luz@satc.edu.br>
2019-01-09 18:29:11 -0200

    'exerciciosreact'

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+    // plugins: [
+    //     new ExtractTextPlugin('app.css')
+    // ],
+    // plugins: ['transform-object-rest-spread']
+
+    // ,
+    //         {
+    //             test: /.css$/,
+    //             loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+    //         }
+module.exports = {
+    entry: './ex/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8081,
+        contentBase: './public'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: 'node_modules',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
